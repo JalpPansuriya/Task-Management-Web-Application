4dc55e088557f04f10afe6035cb6719e87b91d8a
JalpPansuriya <jalppansuriya48@gmail.com>
2026-01-31 17:09:26 +0530

    Update API URL to Render production

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://task-management-web-application-a09g.onrender.com/api';
 
 const getAuthHeader = () => {
     const token = localStorage.getItem('token');
